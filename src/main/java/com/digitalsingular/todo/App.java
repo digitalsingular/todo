e68e4f39fec55df2121b768dd928818046f67170
Agustín Ventura <agustinventura@gmail.com>
2019-07-19 11:36:21 +0200

    initial commit with TodoItem

---
+++ src/main/java/com/digitalsingular/todo/App.java
@@ -0,0 +1,13 @@
+package com.digitalsingular.todo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/digitalsingular/todo/model/item/TodoItem.java
@@ -0,0 +1,15 @@
+package com.digitalsingular.todo.model.item;
+
+public class TodoItem {
+
+	private String description;
+
+	public TodoItem(String description) {
+		this.description = description;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+}
