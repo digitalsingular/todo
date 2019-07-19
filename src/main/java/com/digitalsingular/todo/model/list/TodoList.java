ed409a9039a7f3ac6dd9c12f60345fd745f0788a
Agustín Ventura <agustinventura@gmail.com>
2019-07-19 12:16:16 +0200

    Se pueden añadir items a la lista

--- src/main/java/com/digitalsingular/todo/model/list/TodoList.java
+++ src/main/java/com/digitalsingular/todo/model/list/TodoList.java
@@ -1,8 +1,27 @@
 package com.digitalsingular.todo.model.list;
 
+import java.util.List;
+
+import com.digitalsingular.todo.model.item.TodoItem;
+import com.google.common.collect.ImmutableList;
+
 public class TodoList {
 
-	public int size() {
+	private TodoItem pending;
+	
+	public int getNumberOfPending() {
+		return 0;
+	}
+
+	public void add(TodoItem item) {
+		this.pending = item;
+	}
+
+	public List<TodoItem> getPending() {
+		return ImmutableList.of(pending);
+	}
+
+	public int getNumberOfCompleted() {
 		return 0;
 	}
 
--- src/test/java/com/digitalsingular/todo/model/list/TodoListTests.java
+++ src/test/java/com/digitalsingular/todo/model/list/TodoListTests.java
@@ -1,17 +1,39 @@
 package com.digitalsingular.todo.model.list;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 import org.junit.Test;
+
+import com.digitalsingular.todo.model.item.TodoItem;
 
 public class TodoListTests {
 
 	@Test
-	public void givenANewListShouldContainNoItems() {
+	public void givenANewListShouldContainNoPendingItems() {
 		//Act
 		TodoList list = new TodoList();
 		//Assert
-		assertThat(list.size(), is(0));
+		assertThat(list.getNumberOfPending(), is(0));
+	}
+	
+	@Test
+	public void givenANewListShouldContainNoCompletedItems() {
+		//Act
+		TodoList list = new TodoList();
+		//Assert
+		assertThat(list.getNumberOfCompleted(), is(0));
+	}
+	
+	@Test
+	public void givenAListAddingANewItemShouldStoreItInPending() {
+		//Arrange
+		TodoList list = new TodoList();
+		TodoItem item = new TodoItem("My new item");
+		//Act
+		list.add(item);
+		//Assert
+		assertThat(list.getPending(), hasItem(item));
 	}
 }
