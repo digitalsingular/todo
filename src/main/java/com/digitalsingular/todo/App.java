43effa9e887d47acab8d145a277197c845dd4638
Agustín Ventura <agustinventura@gmail.com>
2019-08-08 20:12:34 +0200

    Añade ejemplo de i18n

--- src/main/java/com/digitalsingular/todo/App.java
+++ src/main/java/com/digitalsingular/todo/App.java
@@ -1,15 +1,18 @@
 package com.digitalsingular.todo;
 
-import java.util.Map;
+import java.util.Locale;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfiguration.class,
-				TestAppConfiguration.class);
-		Map<String, String> databaseProperties = (Map<String, String>) ctx.getBean("databaseProperties");
-		System.out.println(databaseProperties);
+		ApplicationContext ctx = new AnnotationConfigApplicationContext(AppConfiguration.class);
+		String defaultMessage = ctx.getMessage("message", null, "", null);
+		System.out.printf("El mensaje con las locales del sistema es %s%n", defaultMessage);
+		String englishMessage = ctx.getMessage("message", null, "", Locale.ENGLISH);
+		System.out.printf("El mensaje en inglés es %s%n", englishMessage);
+		String spanishMessage = ctx.getMessage("message", null, "", new Locale("es"));
+		System.out.printf("El mensaje en español es %s%n", spanishMessage);
 	}
 }
--- src/main/java/com/digitalsingular/todo/AppConfiguration.java
+++ src/main/java/com/digitalsingular/todo/AppConfiguration.java
@@ -1,31 +1,17 @@
 package com.digitalsingular.todo;
 
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-
-import com.google.common.collect.Maps;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
-@PropertySource("classpath:application.properties")
 public class AppConfiguration {
 
-	@Value("${jdbc.driverClassName}")
-	private String driver;
-
-	@Autowired
-	private Environment environment;
-
 	@Bean
-	public Map<String, String> databaseProperties() {
-		Map<String, String> databaseProperties = Maps.newHashMap();
-		databaseProperties.put("driver", driver);
-		databaseProperties.put("url", environment.getProperty("jdbc.url"));
-		return databaseProperties;
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasenames("messages/messages");
+		return messageSource;
 	}
 }
